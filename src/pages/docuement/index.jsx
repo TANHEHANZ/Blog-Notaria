5670bc08dd5c0e848781352fca7c45f17bec8941
TANHEHANZ <hantach10@gmail.com>
2024-11-22 11:12:08 -0400

    resposive

--- src/pages/docuement/index.jsx
+++ src/pages/docuement/index.jsx
@@ -11,6 +11,7 @@ const Document = () => {
   const volver = () => {
     navigate("/");
   };
+
   if (!documentData) {
     return <div>Documento no encontrado</div>;
   }
@@ -18,21 +19,21 @@ const Document = () => {
   const imageDoc = documentData.documentos.find((doc) => doc.type === "image");
 
   return (
-    <div className=" w-screen h-screen flex  items-start flex-col px-16">
-      <div className=" flex  justify-between w-full mt-[20vh] py-4">
-        <h1 className=" font-bold  uppercase  ">Detalle de documento </h1>
+    <div className="w-full h-screen flex items-start flex-col px-4 sm:px-8 md:px-16">
+      <div className="flex justify-between w-full mt-[20vh] py-4">
+        <h1 className="font-bold uppercase text-sm sm:text-xl md:text-2xl">Detalle de documento</h1>
         <button
-          className="px-4 h-10 bg-primary flex justify-center items-center rounded-md"
+          className="px-4 h-10 bg-primary flex justify-center items-center rounded-md text-white"
           onClick={volver}
         >
-          volver
+          Volver
         </button>
       </div>
 
-      <div className="h-[65%] w-full flex justify-center items-center bg-white rounded-2xl overflow-hidden ">
-        <section className="flex-grow p-16 ">
-          <h1 className="text-3xl font-bold">{documentData.title}</h1>
-          <p>{documentData.contenido}</p>
+      <div className="h-auto sm:h-[80%] md:h-[65%] w-full flex flex-col md:flex-row justify-center items-center bg-white rounded-2xl overflow-hidden">
+        <section className="flex-grow p-4 sm:p-8 md:p-16 text-sm sm:text-base md:text-lg">
+          <h1 className="text-lg sm:text-3xl md:text-3xl font-bold">{documentData.title}</h1>
+          <p className="py-2 md:text-sm ">{documentData.contenido}</p>
 
           {documentData.documentos.map((doc, index) => (
             <div key={index}>
@@ -40,11 +41,10 @@ const Document = () => {
                 <>
                   <strong>{doc.fileName}</strong>
                   <a
-                    key={index}
                     href={doc.name}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className=" h-10 bg-primary flex justify-center items-center rounded-md gap-4 text-white"
+                    className="h-10 bg-primary flex justify-center items-center rounded-md gap-4 text-white mt-2"
                   >
                     <p>Descargar</p> <VscFilePdf color="white" />
                   </a>
@@ -53,11 +53,12 @@ const Document = () => {
             </div>
           ))}
         </section>
+
         {imageDoc && (
           <img
             src={imageDoc.name}
             alt={documentData.title}
-            className=" w-1/2 h-full object-cover "
+            className="w-full md:w-1/2 h-full object-cover rounded-md"
           />
         )}
       </div>
--- src/pages/home/sections/header.jsx
+++ src/pages/home/sections/header.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full h-[70dvh]  flex flex-col justify-center items-start overflow-hidden relative p-16">
-      <h2 className="text-xl font-bold uppercase ">
+    <header className="w-full h-[70dvh] flex flex-col justify-center items-start overflow-hidden relative p-8 sm:p-12 md:p-16">
+      <h2 className="text-sm sm:text-xl md:text-2xl font-bold uppercase">
         - Aprende sobre documentos notariales -
       </h2>
-      <h1 className="text-8xl font-bold text-primary">Mundo Notarial</h1>
-      <p className="text-xl font-medium ">
-        En este blog abrarca un enfoque amplio de temas notariales.
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-primary">
+        Mundo Notarial
+      </h1>
+      <p className="text-sm sm:text-xl md:text-2xl font-medium">
+        En este blog abarca un enfoque amplio de temas notariales.
       </p>
     </header>
   );
